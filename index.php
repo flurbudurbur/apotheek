a66b3872b471f6274e3eaea6c5ce5d79546e6542
flurbudurbur <sebastianhoogendijk@gmail.com>
2022-02-07 10:57:32 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include './pages/head.php' ;?>
+        <title>Schut Huisartsen praktijk</title>
+        <meta name="desctiption" content="placeholder text :D">
+    </head>
+    <body>
+        <header>
+            <?php include './pages/header.php' ;?>
+        </header>
+    </body>
+</html>
---
+++ pages/header.php
@@ -0,0 +1,6 @@
+<nav>
+    <img src="../img/placeholder-logo.png" alt="logo">
+    <div>
+        
+    </div>
+</nav>
