a4878740a43ec1896862289d7e4b3638a4a04c28
Sebastian Hoogendijk <sebastianhoogendijk@gmail.com>
2022-02-15 12:17:01 +0100

    push this i wanna eat

--- index.php
+++ index.php
@@ -12,17 +12,9 @@
         <article>
             <div class="gallery">
                 <div class="slideshow">
-                    <!-- I LEFT OFF HERE -->
                     <!-- IMAGE AND SLIDESHOW SHIT HERE! -->
                 </div>
-                <div class="mapouter">
-                    <div class="gmap_canvas">
-                        <iframe width="300" height="300" id="gmap_canvas" src="https://maps.google.com/maps?q=hofstraat%2013,%20Schagen&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-                        <a href="https://putlocker-is.org">putlocker</a>
-                        <br>
-                        <a href="https://www.embedgooglemap.net">interactive google map for website</a>
-                    </div>
-                </div>
+                <iframe class="gmaps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d782.3454723922455!2d4.794543678080574!3d52.78480398905438!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47cf4e258ca8ccdb%3A0x3272ca6e663a9bac!2sROC%20Kop%20van%20Noord-Holland!5e0!3m2!1sen!2sde!4v1644918898475!5m2!1sen!2sde" allowfullscreen="true" loading="lazy"></iframe>
             </div>
         </article>
     </body>
