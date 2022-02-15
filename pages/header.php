f00f41afdff94efd6683f06d4eac315f53eb43ad
Sebastian Hoogendijk <sebastianhoogendijk@gmail.com>
2022-02-15 12:17:26 +0100

    do the thiiiing

--- pages/header.php
+++ pages/header.php
@@ -1,5 +1,8 @@
 <nav>
-    <img class="logo" src="img/logo.png" alt="logo">
+    <div class="title">
+        <img class="logo" src="img/logo.png" alt="logo">
+        <h1>Schut Huisartsen Praktijk</h1>
+    </div>
     <div class="menu">
         <li>
             <a class="btn" href="#">Home</a>
