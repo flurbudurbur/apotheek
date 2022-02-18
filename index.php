901758ff0c3ada5637ac4dcd5567d6c901e76005
Sebastian Hoogendijk <sebastianhoogendijk@gmail.com>
2022-02-18 09:34:44 +0100

    laptop to desktop push

--- index.php
+++ index.php
@@ -16,6 +16,11 @@
                 </div>
                 <iframe class="gmaps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d782.3454723922455!2d4.794543678080574!3d52.78480398905438!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47cf4e258ca8ccdb%3A0x3272ca6e663a9bac!2sROC%20Kop%20van%20Noord-Holland!5e0!3m2!1sen!2sde!4v1644918898475!5m2!1sen!2sde" allowfullscreen="true" loading="lazy"></iframe>
             </div>
+            <div class="service">
+                <a href="">Herhaalservice</a>
+                <a href="">Medicijnoverzicht</a>
+                <a href="">Inschrijven</a>
+            </div>
         </article>
     </body>
 </html>
